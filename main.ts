0e315acb3b6fded7814d884c66ae2739b0f88605
BodnarJanos <bodnar.janos@hotmail.com>
2023-12-25 21:12:08 +0100

    main.ts frissítése

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@
  * generated for an **exported** function.
  */
 
-//% color="#AA278D" weight=1000
+//% color="#AA278D" weight=100
 namespace hello {  // ez jelenik meg a menüben
     //% block
     export function helloWorld() {  // ez lesz ez első blokk utasítás

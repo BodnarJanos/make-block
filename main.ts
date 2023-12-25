5c8bbe45696480c65d6299001686af3434517391
BodnarJanos <bodnar.janos@hotmail.com>
2023-12-25 23:56:19 +0100

    main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,4 +1,50 @@
 /**
+ * Use the VARIABLE.defl=VALUE notation to specify
+ * default argument values.
+ */
+
+//% color="#BB288D"
+namespace test {
+    //% block
+    //% x.defl=42
+    export function foo(x: number) {
+
+    }
+
+    //% block
+    //% s.defl="hi!"
+    export function bar(s: string) {
+
+    }
+}
+
+
+/**
+ * For better readability, you should rewrite the function
+ * signature in a "natural language" way. You can refer to
+ * each variable in any order using the $VARIABLE syntax.
+ */
+
+//% color="#AA278D" wight=100
+namespace formatting {
+
+    //% block="fooooooo"
+    export function foo() {
+
+    }
+
+    //% block="bar x = $x text = $text"
+    export function bar(x: number, text: string) {
+
+    }
+
+    //% block="shuffled text = $text x = $x"
+    export function shuffled(x: number, text: string) {
+
+    }
+}
+
+/**
  * Functions are mapped to blocks using various macros
  * in comments starting with %. The most important macro
  * is "block", and it specifies that a block should be
@@ -21,21 +67,3 @@ namespace hello {
     }
 }
 
-//% color="#AA278D" wight=100
-namespace formatting {
-
-    //% block="fooooooo"
-    export function foo() {
-
-    }
-
-    //% block="bar x = $x text = $text"
-    export function bar(x: number, text: string) {
-
-    }
-
-    //% block="shuffled text = $text x = $x"
-    export function shuffled(x: number, text: string) {
-
-    }
-}
